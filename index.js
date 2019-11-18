eb9a972b5420278fbc00bf78e6aad75fc10dd961
verodant <verodant@gmail.com>
2019-11-18 10:11:18 +0100

    1 file modified

--- index.js
+++ index.js
@@ -5,27 +5,15 @@ console.log(container.children[container.children.length - 1])
 const actualR = container.children[container.children.length - 1];
 const actualL = container.children[1];
 
-
 const options = {
   root: container,
   rootMargin: "50%",
   threshold: [.5]
 };
 
-const ways = {
-  left: true,
-  right: true
-};
+const ways = {};
 
-const callback = entries =>
-  entries.forEach(entry => {
-    const DIRECTION = entry.boundingClientRect.left > 0 ? "left" : "right";
-        console.dir('->',entry);
-    ways[DIRECTION] = !ways[DIRECTION];
-    console.log(ways)
-  });
-
+const callback = entries =>entries.forEach(entry => ways[entry.target] = !ways[entry.target]);
 observer = new IntersectionObserver(callback, options);
-
 observer.observe(actualR);
 observer.observe(actualL);
